36b0ba43d6975743a6bff6ef2cb2ca2a807e559c
Petro Franko <petrofranko@gmail.com>
2012-08-17 01:11:55 +0300

    change of ui on the main page

--- views/index.tmpl.php
+++ views/index.tmpl.php
@@ -6,6 +6,27 @@
 
 <div id="map"></div>
 
+	<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="#">autostop</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+            <p class="navbar-text pull-right">Logged in as <a href="#">username</a></p>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
 <div id="tabs">
 	<ul>
 		<li><a href="#tab1">Додати маршрут</a></li>
