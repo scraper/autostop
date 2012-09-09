2cb7d390d9d1f3637a66ca791f61a8c9317a85a6
Petro Franko <petrofranko@gmail.com>
2012-09-09 23:39:58 +0300

    removed commented code

--- search.php
+++ search.php
@@ -6,7 +6,3 @@ if (isset($_POST['query'])) {
 	$res = search($_POST['query']);
 	echo json_encode($res);
 };
-// $res = search("l");
-// $result[] = $res;
-// echo json_encode(array("name"=>"John","time"=>"2pm"));
-// echo json_encode($result);
