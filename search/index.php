80a4fd41107985d8f8f18d7d7953c0015011e657
Petro Franko <petrofranko@gmail.com>
2013-04-25 00:31:14 +0300

    advanced search

--- search/index.php
+++ search/index.php
@@ -11,4 +11,12 @@ elseif ($_GET['q'] != null) {
 	$q = $_GET['q'];
 	$res = array('objA'=>typeahead_search($q), 'objB'=>search($q), 'objB'=>search($q));
 	echo json_encode($res);
+}
+elseif (isset($_POST['isAdvanced'])) {
+	$q = $_POST['query'];
+	$s_date = $_POST['s_date'];
+	$e_date = $_POST['e_date'];
+	$type = $_POST['type'];
+	$res = array('objB'=>advanced_search($s_date, $e_date, $type, $q));
+	echo json_decode($res);
 };
