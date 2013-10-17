ca1791c6ea47e98f4ca407552661e9a25ccca2a8
Petro Franko <petrofranko@gmail.com>
2013-10-18 00:51:22 +0300

    new2.tmpl.php, search.tmpl.php
    no special changes

--- views/search.tmpl.php
+++ views/search.tmpl.php
@@ -75,6 +75,17 @@
 
 					<div class="result"></div>
 				</div>
+				<div class="pagination pagination-centered">
+					<ul>
+						<li><a href="#">Prev</a></li>
+						<li><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li><a href="#">Next</a></li>
+					</ul>
+				</div>
 			</div>
 		</div><!--/span-->
 
